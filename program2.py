673d6128c9bc0889267d7b4c5d5d165c0a72abed
Veduzzz2013 <vedaantranjan83@gmail.com>
2026-02-27 18:27:14 +0530

    Basic Programs

---
+++ program2.py
@@ -0,0 +1,11 @@
+# to input length and breadth of rectangle and calculate area
+
+length=float(input("Enter length of the rectagle: "))
+breadth=float(input("Enter breadth of the rectangle: "))
+area=length*breadth
+
+print("******Rectangles Specification******")
+print("Length= ",length)
+print("Breadth= ",breadth)
+print("Area= ",area)
+print("******End******")
---
+++ program4.py
@@ -0,0 +1,4 @@
+name='Vedaant'
+age=12
+print(name,",you are",age,"now but")
+print("you will be", age+1,"on June 25th")
