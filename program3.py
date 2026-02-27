fd34d1f8e2de6378b9affbe26f5a4b814b96608d
Veduzzz2013 <vedaantranjan83@gmail.com>
2026-02-27 18:31:30 +0530

    Basic Programs

--- program3.py
+++
@@ -1,7 +0,0 @@
-# celsius to Farenheit
-
-cels=float(input("Enter temp in Celsius: "))
-print("Temperature in Celsius is: ",cels)
-
-fahr=cels*9/5+32                  
-print("Temperature in Fahrenheit is: ",fahr)
--- program4.py
+++
@@ -1,5 +0,0 @@
-name='Vedaant'
-age=12
-print(name,",you are",age,"now but")
-
-print("you will be", age+1,"on June 25th")
