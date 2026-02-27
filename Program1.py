c5c80b6b655cbbe289a5f0a24ef8e9eeb693cd77
Veduzzz2013 <vedaantranjan83@gmail.com>
2026-02-27 18:32:17 +0530

    Basic Program

---
+++ Program1.py
@@ -0,0 +1,10 @@
+# to input 3 numbers and print their sum
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+num3 = int(input("Enter third number: "))
+
+sum = num1 + num2 + num3
+
+print("the numbers are: ",num1,",",num2,",",num3)
+print("Their sum is:", sum)
